750197ff9651dae7f029de712b59644fd7ad8d0b
joseph94 <josephyongliongkang@hotmail.com>
2017-02-22 15:41:53 +0800

    learner

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,11 @@
+require 'digest'
+
 class Url < ApplicationRecord
+	validates :long_url, presence: true
+	# This is Sinatra! Remember to create a migration!
+	def self.shorten(web)    
+		short = Digest::SHA1.hexdigest(web)[8..13]
+		latest = Url.find_by(long_url: web)
+		latest.update(short_url: short)
+	end
 end
